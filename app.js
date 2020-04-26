929f7441190a910d8b91c32b67ee5e947ac6fb75
Ignacio <shoik.xtazy@gmail.com>
2020-04-26 17:17:38 -0400

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const importFunc = require('./functions.js')
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.get('/portfolio', function (req, res) {
 
 app.use('/assets', express.static(path.join(__dirname, "./assets")));
 app.use('/js', express.static(path.join(__dirname, "./js")));
+app.use(cors());
 
 const fromAnotherFile = require("./functions")
 app.get('/', function (req, res) {
