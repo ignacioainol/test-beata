53b244432f94a371963deaba789cd21a337a99c5
Ignacio <shoik.xtazy@gmail.com>
2020-04-26 17:23:37 -0400

    fdsf

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const importFunc = require('./functions.js')
-var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,7 +23,6 @@ app.get('/portfolio', function (req, res) {
 
 app.use('/assets', express.static(path.join(__dirname, "./assets")));
 app.use('/js', express.static(path.join(__dirname, "./js")));
-app.use(cors());
 
 const fromAnotherFile = require("./functions")
 app.get('/', function (req, res) {
